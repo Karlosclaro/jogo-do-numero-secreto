2678fa61072ae5c9959d44813de068089f3c89c2
carlosklaro <carlosklaro@gmail.com>
2025-02-08 03:40:18 +0000

    elementos fundamentais completados

--- js/app.js
+++ js/app.js
@@ -1,9 +1,60 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function verificarChute() {
-    console.log('O botão foi clicado');
+    let chute = document.querySelector('input').value
+    // console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+      exibirTextoNaTela('h1', "Acertou!");
+      let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+      let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+      exibirTextoNaTela('p', mensagemTentativas);
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2175372212.
+      document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+      if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', 'O número secreto é menor');
+      } else {
+        exibirTextoNaTela('p', 'O número secreto é maior');
+      }
+      // tentativas = tentativas + 1
+      tentativas++
+      limparCampo();
+    }
+}
+
+function exibirTextoNaTela(tag, texto){
+let campo = document.querySelector(tag);
+campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela('h1', 'Jogo do número secreto' );
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+
+function gerarNumeroAleatorio(){
+  return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3480326537.
+function reiniciarJogo(){
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
 }
